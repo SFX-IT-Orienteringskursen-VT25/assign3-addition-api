8113ff47422b027850c5f06939098a8808364157
JinHua Lin <jinhua.lin@utb.tyreso.se>
2025-05-19 11:21:20 +0200

    Update Program.cs

--- Server/AdditionApi/Program.cs
+++ Server/AdditionApi/Program.cs
@@ -1,34 +1,29 @@
-using AdditionApi;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 添加服务
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo
     {
-        Title = "Addition API",
+        Title = "Storage API",
         Version = "v1"
     });
 });
 
 var app = builder.Build();
 
-// 配置 HTTP 请求管道
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Addition API V1");
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Storage API V1");
     });
 }
 
 app.UseHttpsRedirection();
-app.MapControllers(); // 启用 Controller 路由
-
+app.MapControllers();
 app.Run();
