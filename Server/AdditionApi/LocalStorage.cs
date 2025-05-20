db0ddfb6114fb7151b9be40b996c40cb4004d7f8
SafirJMT <safir.manghat@utb.tyreso.se>
2025-05-20 03:11:03 +0200

    localStorageApi

---
+++ Server/AdditionApi/LocalStorage.cs
@@ -0,0 +1,5 @@
+public class LocalStorage {
+    public required string key { get; set; }
+    public required string value { get; set; }
+
+}
--- Server/AdditionApi/Program.cs
+++ Server/AdditionApi/Program.cs
@@ -1,34 +1,37 @@
-using AdditionApi;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.OpenApi.Models;
+var container = new List<LocalStorage>
+{
+    new LocalStorage { key = "John", value = "SDE" },
+    new LocalStorage { key = "Owilsson", value = "Platform Engineer" }
+};
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 添加服务
-builder.Services.AddControllers();
+//Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Addition API",
-        Version = "v1"
-    });
-});
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-// 配置 HTTP 请求管道
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Addition API V1");
-    });
-}
+    app.UseSwaggerUI();
+};
 
 app.UseHttpsRedirection();
-app.MapControllers(); // 启用 Controller 路由
+app.MapGet("/getData", () => {
+
+    return Results.Ok(localstorage);
+});
+
+
+app.MapPost("/postData", ([FromBody] LocalStorage localstorage, HttpContext context) => {
+    container.Add(localstorage)
+    return Results.Created(localstorage);
+});
+
 
 app.Run();
