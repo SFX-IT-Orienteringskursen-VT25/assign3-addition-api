e60ca373ef8655a466aeb706410b6f46c641c1fb
Maya Matueva <mayya.matueva@utb.tyreso.se>
2025-05-24 02:01:03 +0200

    removed all example code related to order and weatherforecast

--- Server/AdditionApi/Program.cs
+++ Server/AdditionApi/Program.cs
@@ -21,35 +21,4 @@ app.MapGet("/storage/{key}", ([FromRoute] string key) =>
     return Results.Ok(new StorageValue { Value = value });
 });
 
-// WeatherForecast endpoint
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast(
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            WeatherForecastStatus.Summaries[Random.Shared.Next(WeatherForecastStatus.Summaries.Length)]
-        ))
-    .ToArray();
-    return forecast;
-});
-
-// Order endpoints
-app.MapPost("/order", ([FromBody] Order order) =>
-{
-    if (string.IsNullOrEmpty(order.Item))
-        return Results.BadRequest("Must provide an item");
-    return Results.Ok("Order received");
-});
-
-app.MapPut("/order", ([FromBody] Order order) =>
-{
-    return Results.Ok("Order has been updated");
-});
-
-app.MapDelete("/order", ([FromBody] Order order) =>
-{
-    return Results.NoContent();
-});
-
 app.Run();
