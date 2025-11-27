3a7cbd36b8af31de018a2a94f4ce5946164e3311
junzhou-aaa <nkuzhoujun@126.com>
2025-11-27 09:16:36 +0800

    add new endpoints and client html

--- Server/AdditionApi/Program.cs
+++ Server/AdditionApi/Program.cs
@@ -1,55 +1,36 @@
-using AdditionApi;
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()  
+              .AllowAnyMethod()  
+              .AllowAnyHeader();  
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Run new Cors
+app.UseCors();
+
+// Create new var in memory
+int currentNumber = 0;
+
+app.MapGet("/number", () =>
 {
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-
-
-app.MapGet("/", () =>
-{
-    return "Hello World!";
+    return Results.Ok(currentNumber);
 });
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                Random.Shared.Next(-20, 55),
-                WeatherForecastStatus.Summaries[Random.Shared.Next(WeatherForecastStatus.Summaries.Length)]
-            ))
-        .ToArray();
-    return forecast;
-});
-
-app.MapPost("/order", ([FromBody] Order order) =>
-{
-    if (order.Item == null)
-    {
-        return Results.BadRequest("Must provide an item");
+app.MapPost("/number", (int number) =>
+{{
+        // update new number in memory
+        currentNumber = number;
     }
+    return Results.Ok("Number received");
+});
 
-    return Results.Ok("Order received");
-});
-app.MapPut("/order", ([FromBody] Order order) =>
-{
-    return Results.Ok("Order has been updated");
-});
-app.MapDelete("/order", ([FromBody] Order order) => Results.NoContent());
 
 app.Run();
